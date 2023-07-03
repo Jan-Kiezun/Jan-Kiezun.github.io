3184b7f6da8cf5a95da9df4c9fc3bb820f16159d
Jan-Kiezun <j4n3k777@gmail.com>
2023-07-03 19:52:56 +0200

    startView baseline

--- gith-pgs-client/src/App.tsx
+++ gith-pgs-client/src/App.tsx
@@ -1,7 +1,9 @@
+import StartView from "./components/landingPage/startView/StartView";
+
 function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center bg-gray-100">
-      <span className="text-4xl font-bold text-gray-700">Landing page</span>
+      <StartView />
     </div>
   );
 }
---
+++ gith-pgs-client/src/components/landingPage/startView/StartView.tsx
@@ -0,0 +1,12 @@
+﻿import WelcomeButtons from "./WelcomeButtons";
+
+function StartView() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-[100px]">Jan Kieżun</h1>
+      <WelcomeButtons />
+    </div>
+  );
+}
+
+export default StartView;
