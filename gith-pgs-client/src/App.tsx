2d666ad1114cd1616d6d897ecbb279741f7cbf9a
Jan-Kiezun <j4n3k777@gmail.com>
2023-07-03 19:25:17 +0200

    init project

---
+++ gith-pgs-client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center bg-gray-100">
+      <span className="text-4xl font-bold text-gray-700">Landing page</span>
+    </div>
+  );
+}
+
+export default App;
